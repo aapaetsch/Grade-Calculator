18932f3a5632e3db38bf19e8761bf14961109361
Aidan Paetsch <aapaetsch@gmail.com>
2018-03-08 17:33:15 -0700

    some UI and course class

---
+++ GPACalculator/app/src/main/java/com/example/gpacalculator/Course.java
@@ -0,0 +1,44 @@
+package com.example.gpacalculator;
+
+/**
+ * @author Created by Aidan Paetsch
+ * @date 03/08/2018
+ * Class for a singular course
+ */
+
+public class Course {
+	private String courseCode;
+	private String department;
+	private String grade; // X denotes a course that is yet to be graded
+	private Integer year; // year of your degree in which a class was taken
+	private String user; // who took the course, app can be used for multiple users
+	private String semester; // either fall, winter, spring or summer
+	
+	/**
+	* Creates a new Course object
+	* @param courseCode -unique ID of the course
+	* @param department -department ID the course falls under
+	* @param grade -letter grade, can have - or + 
+	* @param user -name of the person who took this course
+	* @param semester -semester of the year in which the course was taken
+	* @param year -year of the degree in which a course is taken
+	*/
+	public Course(String courseCode, String department, String grade, String user, String semester, Integer year){
+		this.courseCode = courseCode;
+		this.department = department;
+		this.grade = grade;
+		this.user = user;
+		this.semester = semester;
+		this.year = year;
+	}
+	// setter for a course, uses the same parameters as course
+	public void setCurrentCourse(String courseCode, String department, String grade, String user, String semester, Integer year){
+		this.courseCode = courseCode;
+		this.department = department;
+		this.grade = grade;
+		this.user = user;
+		this.semester = semester;
+		this.year = year;
+	}
+
+}
---
+++ GPACalculator/app/src/main/java/com/example/gpacalculator/userSelect.java
@@ -0,0 +1,13 @@
+package com.example.gpacalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class userSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_select);
+    }
+}
