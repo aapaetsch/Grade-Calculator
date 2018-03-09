b318acf4c9df267c4624de1eab0ae15665b594c7
Aidan Paetsch <aapaetsch@gmail.com>
2018-03-08 19:59:45 -0700

    starting on login screen

--- GPACalculator/app/src/main/java/com/example/gpacalculator/userSelect.java
+++ GPACalculator/app/src/main/java/com/example/gpacalculator/userSelect.java
@@ -1,7 +1,10 @@
 package com.example.gpacalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class userSelect extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class userSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_select);
+
+    }
+    public void enterUser(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
